03856761d32cfd74da7ead1e6efad9782188fc3a
VANSH-AGRAWAL007 <110198854+VANSH-AGRAWAL007@users.noreply.github.com>
2022-09-05 23:13:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,6 @@ from home_window import *
 frames=[]
 
 customer_dat ={}
-def custfresher():
-	global customer_dat
-	with open('Data_files\\customer_file.csv',newline='\n') as cust:
-		alldata = csv.reader(cust,delimiter=',')
-		for i in alldata:
-			customer_dat[i[0]] = i[1]
-			
-custfresher()
-
 
 if __name__ == "__main__":
 	h = home()
